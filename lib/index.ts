26c007be913adac01c48037f2162e58e75d1b1c1
andrew <andrewxhill@gmail.com>
2019-02-26 12:09:37 -0800

    bump version

--- lib/index.ts
+++ lib/index.ts
@@ -7,9 +7,20 @@ import {
 
 const { CameraRollEvents } = NativeModules
 
-export const eventEmitter = Platform.select({
-  android: DeviceEventEmitter,
-  ios: new NativeEventEmitter(CameraRollEvents)
-})
+export interface ILocalPhotoResult {
+  assetId: string,
+  creationDate: string,
+  modificationDate: string,
+  orientation: number,
+  path: string,
+  uri: string,
+  canDelete: boolean
+}
+
+// TODO: change api function to return array of results
+// export const eventEmitter = Platform.select({
+//   android: DeviceEventEmitter,
+//   ios: new NativeEventEmitter(CameraRollEvents)
+// })
 
 export * from './API'
