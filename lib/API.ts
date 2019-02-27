b1e9277acee1dee1d8040f15f8397ca59cb03093
Aaron Sutula <aaron.sutula@gmail.com>
2019-02-26 21:42:18 -0600

    Resolve promise with array of photo data

--- lib/API.ts
+++ lib/API.ts
@@ -2,6 +2,16 @@ import { NativeModules } from 'react-native'
 
 const { RNCameraRoll } = NativeModules
 
-export async function requestLocalPhotos(minEpoch: number): Promise<void> {
+export interface LocalPhotoResult {
+  assetId: string,
+  creationDate: string,
+  modificationDate: string,
+  orientation: number,
+  path: string,
+  uri: string,
+  canDelete: boolean
+}
+
+export async function requestLocalPhotos(minEpoch: number): Promise<LocalPhotoResult[]> {
   return await RNCameraRoll.requestLocalPhotos(Math.round(minEpoch / 1000))
 }
